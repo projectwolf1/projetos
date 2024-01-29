10e7d5530585a2b8f53f584df64e87bd7591cdfd
Guilherme Almeida da Silva <50052882+projectwolf1@users.noreply.github.com>
2024-01-29 20:36:35 -0300

    Update data-nascimento.js

--- data-nascimento.js
+++ data-nascimento.js
@@ -1,22 +1,34 @@
-const dia = 16;
-const metros = 20;
+//quantos metros precisam ser percorridos ? 
+const finalMetragem =  480;
 
-const dia2 = 0;
+//primeira tartaruga
+const dia = 16;
+const metros = 30;
+
+//segunda tartaruga
 const metros2 = 20; 
+
+//soma da metragem das duas tartarugas
 const chegada = dia * metros
+
+    
+        function descobreDiasTartaruga2 (dias2, resultado) {
+            var dias2 = (finalMetragem / metros2)
+            const resultado =  dias2 - dia
+
+            console.log(resultado)
+
+            return resultado
+        }
+
 
 
     const MultiplicaMetroTartaruga = function () {
 
-        if ( chegada > 400){
-            console.log(`A primeira tartaruga chegou até o destino depois de percorrer foi de ${chegada} Metros. Parabéns!!`);
-         }else{
-            
-        if (chegada < 400)
-            console.log(`A primeira tartaruga chegou até o destino depois de percorrer foi de ${chegada} Metros. Se esforce mais!!`); 
-         }
-            
-
+            if ( chegada >= finalMetragem){
+                console.log(`A primeira tartaruga chegou até o destino depois de percorrer foi de ${chegada} Metros. Parabéns!!`);
+            }
 }
 
-console.log(MultiplicaMetroTartaruga());
+MultiplicaMetroTartaruga(chegada);
+descobreDiasTartaruga2(resultado)
