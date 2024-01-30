4b8a67d9dcaf6f0fe73785b24e38b39a69833ecd
Guilherme Almeida da Silva <50052882+projectwolf1@users.noreply.github.com>
2024-01-30 20:51:57 -0300

    Add files via upload

---
+++ negando.js
@@ -0,0 +1,15 @@
+const afirmação = "Se hoje é sábado, amanhã não trabalharei"
+
+
+
+
+function confirmaANegacao () {
+if (!afirmação === "Hojé  é sabado e amanhã trabalharei") {
+    return console.log(true)
+}else {
+   return false
+}
+
+}
+
+console.log(confirmaANegacao());
---
+++ tartaruga.js
@@ -0,0 +1,35 @@
+const linhaDeChegada = 480;
+const dias = '';
+const metros = '';
+const resultado = '';
+
+    function somaAPrimeiraTartaruga (dias, metros) {
+            metros = 30;
+            dias = 16;
+
+        if (metros * dias === linhaDeChegada ) {
+            console.log(`A primeira tartaruga percorreu ${linhaDeChegada} metros, em ${dias} dias até a linha de chegada !`);
+        }
+    }
+
+    somaAPrimeiraTartaruga(dias, metros);
+
+    function quantoTempoASegundaTartarugaLevou (metros, resultado) {
+            metros = 20;
+
+            if  (metros / linhaDeChegada === 24) {
+                resultado =  24 - 16 
+                }
+          console.log(`A segunda tartaruga levou ${resultado} dias a mais que a primeira tartaruga para finalizar a corrida ! `)  
+        }
+
+    quantoTempoASegundaTartarugaLevou();
+
+
+
+
+
+
+
+
+
