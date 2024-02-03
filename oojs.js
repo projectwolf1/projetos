78129dacd61559051da4c7f591284e155b9d509b
Guilherme Almeida da Silva <50052882+projectwolf1@users.noreply.github.com>
2024-02-03 11:11:36 -0300

    Add files via upload

---
+++ oojs.js
@@ -0,0 +1,48 @@
+/* 
+const person = {
+    name : {
+        primeiro: "Guilherme",
+        segundo: "Almeida",
+    },
+    age: 27,
+    gender: "He",
+    hobbies: ["music", "code"],
+    bio: function () {
+        alert (
+            this.name[0] + 
+            " " +
+            this.name[1] + 
+
+            " has " +
+            this.age + 
+            ` years old. He likes ` + 
+            this.hobbies[0] +
+            " and " +
+            this.hobbies[1] +
+            ".",
+        )
+    },
+
+    salutation: function () {
+        alert("Hi ! I am" + this.name[0] + ".");
+    },
+};
+
+
+person.name;
+person.name[0];
+
+
+ */
+
+
+const aluno = {
+    nome: ["Guilherme", "Almeida"],
+    matricula: "111452",
+    curso: "Ciências da Computação",
+    materias: ["Logica de programação", "JavaScript", "História da computação"]
+};
+
+
+
+console.log(aluno)
